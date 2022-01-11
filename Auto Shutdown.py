1fa97ed880b7e2d80f90b8a489b976b270bc8b07
Foxtrot-813 <84441705+Foxtrot-813@users.noreply.github.com>
2022-01-11 01:04:44 +0100

    Update Auto Shutdown.py

--- Auto Shutdown.py
+++ Auto Shutdown.py
@@ -1,21 +1,24 @@
 import os
-
+import time
 choice = input("Please choose one of the options:\n1- Shutdown the PC.\n2- Restart the PC.\n3- Sign out.\n4- Quit\n")
-countdown = input("Please input the countdown time in seconds: ")
+countdown = input("Please input the countdown time in minutes: ")
+
 
 shutdown = "shutdown /s /t "
 restart = "shutdown /r /t "
-logout = "shutdown /l /t "
+logout = "shutdown /l"
 
 try:
+    timeout = int(countdown) * 60
     if choice == "4":
         exit()
     elif choice == "1" and int(countdown):
-        os.system(shutdown + countdown)
+        os.system(shutdown + str(timeout))
     elif choice == "2" and int(countdown):
-        os.system(restart + countdown)
+        os.system(restart + str(timeout))
     elif choice == "3" and int(countdown):
-        os.system(logout + countdown)
+        time.sleep(timeout)
+        os.system(logout)
     else:
         print("Only numbers from 1-4")
 except ValueError:
